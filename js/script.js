417c967dda1428aa8d7c7a3ca39efe7f74678463
insidedacoolest <89404199+insidedacoolest@users.noreply.github.com>
2025-07-15 20:41:34 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 let data = {};
 
-function saveDesignAndGo() {
-    saveDesign(); // guarda o design selecionado
-    window.location.href = 'textil.html'; // avança para próxima página
+function saveDesign() {
+    data.design = document.getElementById('design').value;
+    localStorage.setItem('faturacaoData', JSON.stringify(data));
 }
 
 function saveTextil(textil) {
